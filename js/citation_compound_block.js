f0760cf5e17f7ed0bac8a121ce8263e0c82b6249
nate-rcl <nsarr@library.rochester.edu>
2016-02-08 14:48:47 -0500

    update to allow for call to get mods information may bragdon

--- js/citation_compound_block.js
+++ js/citation_compound_block.js
@@ -1,14 +1,8 @@
 (function ($) {
-    Drupal.behaviors.rochdora_compound_updater = {
+    Drupal.behaviors.may_bragdon_citation_updater = {
         attach: function (context, settings) {
             // Monkey patch Drupal.settings.islandora_paged_tei_seadragon_update_page
             // to update compound block to ensure we always get the current one.
-
-            // Hide empty sidebar at start.
-            if (!$('.islandora-compound-prev-next').length) {
-                $('.sidebars').hide();
-            }
-
             var old_page_update = Drupal.settings.islandora_paged_tei_seadragon_update_page;
             Drupal.settings.islandora_paged_tei_seadragon_update_page = function (pid, page_number) {
                 // Drop out here if we are the most current request.
@@ -18,27 +12,39 @@
                 
                 //run the original function
                 old_page_update(pid, page_number);
-
-                // Update compound block.
+                
+                // call out to get citation information
                 $.ajax({
-                    url: Drupal.settings.basePath + "rochdora/compound_block/" + pid,
+                    url: Drupal.settings.basePath + "may_bragdon/mods/" + pid,
                     cache: false,
                     success: function(response) {
                         // Drop out here if we are not the most current request.
                         if (pid !== Drupal.settings.islandora_paged_tei_seadragon.current_page) {
                             return;
                         }
-                        // Kill the block.
-                        $('.islandora-compound-prev-next').remove();
-                        $('.sidebars').hide();
-                        // Revive the block.
-                        if (response) {
-                            $('.sidebars').show();
-                            $('#block-islandora-compound-object-compound-navigation').append(response);
+                        if(response.mods && response.mods.Page){
+                            $('#citation_page_number').html(response.mods.Page[0]);
+                        } else {
+                             $('#citation_page_number').html("Not Found");  
                         }
+                    },
+                    error: function(response){
+                        console.log(response);
                     }
                 });
             };
         }
+        
     };
+    
+    //call the page load function after the first page load
+    $(function() {
+        
+        if($("#islandora_paged_tei_seadragon_pager").length) {
+            Drupal.settings.islandora_paged_tei_seadragon_update_page(
+                $("#islandora_paged_tei_seadragon_pager").val(),
+                $("#islandora_paged_tei_seadragon_pager").children("option:selected").text()
+            );
+        }
+    });
 })(jQuery);
