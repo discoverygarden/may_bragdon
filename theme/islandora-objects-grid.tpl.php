a56e0ec86560cb0c948d41fbe92272ee1249a721
nate-rcl <nsarr@library.rochester.edu>
2016-03-23 15:22:30 -0400

    updated with wrapper div

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -9,7 +9,10 @@
  <?php foreach($objects  as $object): ?>
    <div class="diary-page-image-wrapper">
        <div class="islandora-object-thumb"><?php print $object['thumb']; ?></div>
-       <div class="page-number-prefix">p.</div><div class="diary-page-number"><?php print $object['page']; ?></div> 
+       <div class="diary-page-number-wrapper">
+           <div class="page-number-prefix">p.</div>
+           <div class="diary-page-number"><?php print $object['page']; ?></div> 
+       </div>
    </div>
  <?php endforeach; ?>
  <div class="clearfix"></div>   
