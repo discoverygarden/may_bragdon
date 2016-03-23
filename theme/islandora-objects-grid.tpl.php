3fa3b4db0b074a36e91ee7a8cb70139f4bdeca88
nate-rcl <nsarr@library.rochester.edu>
2016-03-23 15:07:33 -0400

    removed styling and increased limit to 25 items per page on pages page

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -7,14 +7,12 @@
 ?>
 <div class="islandora-objects-grid clearfix">
  <?php foreach($objects  as $object): ?>
-   <div class="islandora-objects-grid-item">
-     <dl class="islandora-object <?php print $object['class']; ?>">
-       <dt class="islandora-object-thumb"><?php print $object['thumb']; ?></dt>
-       <dd class="islandora-object-caption"><?php print $object['link']; ?></dd>
-       <dd><div class="page-number-prefix">p.</div><div class="diary-page-number"><?php print $object['page']; ?></div>  </dd>
-     </dl>     
+   <div class="diary-page-image-wrapper">
+       <div class="islandora-object-thumb"><?php print $object['thumb']; ?></div>
+       <div class="page-number-prefix">p.</div><div class="diary-page-number"><?php print $object['page']; ?></div> 
    </div>
  <?php endforeach; ?>
+ <div class="clearfix"></div>   
     
  
 </div>
